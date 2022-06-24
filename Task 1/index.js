2d725d5bf1da714ddfb1e02de703835e789b5ef0
AGNES LILY <97900099+AgnesLily2772@users.noreply.github.com>
2022-06-24 12:27:37 +0530

    Add files via upload

---
+++ Task 1/index.js
@@ -0,0 +1,33 @@
+import data from './events.json' assert { type: "json" };
+
+const search = document.getElementById('serachbox');
+search.addEventListener('keyup',(e)=>{
+    const event = e.target.value.toLowerCase();
+    const filteredData = data.filter(two => {
+        return two.name.toLowerCase().includes(event);
+    })
+    display(filteredData);
+})
+const display = (items)=>{
+    const html = items
+    .map((item)=>{
+        return `
+        <div class="row mydiv">
+        <div class="col">
+        <img class="myimage" src=${item.image}/>
+        </div>
+        <div class="col">
+            <h4 class="myname">${item.name}</h4>
+            <p class="mytype">${item.type}<p/>
+            <p class="mydate">${item.date}<p/>
+            <p class="myseat">Seats Available:${item.SeatsAvailable}<p/>
+            ${(item.SeatsAvailable!=0)?`<button class="btn btn-success">Book Now</button>` :`<button class="btn btn-danger">Sold Out</button>` }
+        </div>
+            <p class="myvenue">Venue : ${item.venue}<p/>
+        </div>
+        `
+    })
+    .join('')
+    document.getElementById('main').innerHTML = html;
+};
+display(data)
